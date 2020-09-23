19df6512adde090eea1a28877a8c39feeb8b69eb
victormdev <victoralexandrino99@gmail.com>
2020-09-23 01:02:56 -0300

    insert graphic captions

--- lines.py
+++ lines.py
@@ -2,8 +2,15 @@
 
 import matplotlib.pyplot as plt 
 
-x = [1, 2]
-y = [3, 4]
+x = [1, 2, 5]
+y = [3, 4, 6]
+
+#Título
+plt.title("Gráfico com Python")
+
+#Eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
